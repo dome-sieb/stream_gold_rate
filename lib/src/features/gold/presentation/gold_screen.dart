56e1139836d95e80db728acfa412894b23658776
dome-sieb <sieberdominique10@gmail.com>
2024-06-24 12:00:28 +0200

    Stream Builder implementiert

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
+    StreamBuilder<double>(
+      stream: getGoldPriceStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data != null) {
+          return Text(
+            NumberFormat.simpleCurrency(locale: 'de_DE').format(snapshot.data),
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.primary),
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
     const double goldPrice = 69.22;
 
     return SafeArea(
@@ -21,8 +38,23 @@ class GoldScreen extends StatelessWidget {
               Text('Live Kurs:',
                   style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
+              StreamBuilder(
+                  stream: getGoldPriceStream(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(
+                        NumberFormat.simpleCurrency(locale: 'de_DE')
+                            .format(snapshot.data),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge!
+                            .copyWith(
+                                color: Theme.of(context).colorScheme.primary),
+                      );
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  }),
               Text(
                 NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
                 style: Theme.of(context)
